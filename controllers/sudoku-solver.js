b529b7a9d70564f858b8c18e56483eff393228e0
bart c <kaszikasz@wp.pl>
2023-07-17 23:22:34 +0100

    in "check" and "solve" routes
    /api/check :
    
    + row placement
    + col placement
    + response
    
    /api/solve :
    
    + validate for tests 3,4,5

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,14 +1,124 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    let error;
+    const numberRegex = /^[\d.]+$/
+    
+    if (puzzleString) {
+      // puzzle greater or less than 81 characters
+      if (puzzleString.length !== 81) {
+        error = 'Expected puzzle to be 81 characters long'
+
+      } else {
+        // not number or period
+        numberRegex.test(puzzleString)
+        ? error = ""
+        : error = 'Invalid characters in puzzle'
+      }
+      
+      // invailid or cannot solve puzzle { error: 'Puzzle cannot be solved' }
+
+    } else {
+      // puzzle missing
+      error = 'Required field missing'
+    }
+
+    return error
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    
+    function createRowArray(str) {
+      const subarrayLength = 9;
+      const rowArray = [];
+    
+      let i = 0;
+      while (i < str.length) {
+        const subarray = str.substring(i, i + subarrayLength);
+        rowArray.push(subarray);
+        i += subarrayLength;
+      }
+    
+      return rowArray;
+    }
 
+    // create array with rows (as subststrings)
+    let rowArray = createRowArray(puzzleString)
+    let rowArrayNr;
+    let validation;
+
+    // switch row letters to numbers - to use them as index numbers for rowArray
+    switch (row.toLowerCase()) {
+      case "a":
+        rowArrayNr = 0
+        break;
+      case "b":
+        rowArrayNr = 1
+        break;
+      case "c":
+        rowArrayNr = 2
+        break;
+      case "d":
+        rowArrayNr = 3
+        break;
+      case "e":
+        rowArrayNr = 4
+        break;
+      case "f":
+        rowArrayNr = 5
+        break;
+      case "g":
+        rowArrayNr = 6
+        break;
+      case "h":
+        rowArrayNr = 7
+        break;
+      case "i":
+        rowArrayNr = 8
+        break;
+    }
+
+    // check if row contains coordinate value
+    rowArray[rowArrayNr].includes(value)
+      ? validation = false
+      : validation = true
+
+      console.log(validation)
+    return validation
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    
+    let arrayOfColumns = []
 
+    function createColArray(str, firstChar) {
+      const charGap = 9;
+      const colArray = [];
+    
+      let i = firstChar;
+
+      while (i < str.length) {
+          colArray.push(str[i])
+          i += charGap;
+      }
+  
+      return colArray;
+    }
+
+    // create array of subarrayas - representing columns, which contain 
+    let i = 0;
+    while (i < 9) {
+      arrayOfColumns.push(createColArray(puzzleString, i))
+      i += 1
+    }
+    
+    // if value is in array, validation = true. if not, validation = false
+    let validation = arrayOfColumns[column-1].some( char => {
+      // console.log(arrayOfColumns[column-1])
+      return char === value
+    })
+
+    return !validation
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
@@ -16,7 +126,7 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
-    
+
   }
 }
 
--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,48 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+      let puzzle = req.body.puzzle
+      let coordinate = req.body.coordinate
+      let rowCoor = coordinate[0]
+      let colCoor = coordinate[1]
+      let value = req.body.value
+
+      let validRow;
+      let validCol;
+      let conflict = [];
+
+      // check rows
+      solver.checkRowPlacement(puzzle, rowCoor, colCoor, value)
+        ? validRow = true
+        : validRow = false
+
+      if (!validRow) conflict.push("row")
+
+      // check columnns
+      solver.checkColPlacement(puzzle, rowCoor, colCoor, value)
+        ? validCol = true
+        : validCol = false
+
+        if (!validCol) conflict.push("column")
+
+      // generate response
+      if (validRow && validCol) res.json({valid: true})
+      else {
+        res.json({
+          valid: false,
+          conflict: conflict
+        })
+      }
 
     });
     
   app.route('/api/solve')
     .post((req, res) => {
+      let puzzle = req.body.puzzle
+      let output = solver.validate(puzzle)
 
+      output 
+        ? res.json({error: output}) 
+        : res.json({error: "no error"})
     });
 };

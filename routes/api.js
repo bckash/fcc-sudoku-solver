934829951ad22551d1566eb432669206fb51c2b3
bart c <kaszikasz@wp.pl>
2023-08-12 22:05:30 +0100

    C:/Program Files/Git/api/check secured for all test errors

--- routes/api.js
+++ routes/api.js
@@ -12,8 +12,10 @@ module.exports = function (app) {
       let coordinate = req.body.coordinate
       let value = req.body.value
 
+      console.log(puzzle)
       console.log("v = "+value)
       console.log("c = "+coordinate)
+      console.log(".............")
 
       const puzzleRegex = /^[\d.]+$/
       const coodinateRegex = /^[A-Ia-i][1-9]$/
@@ -21,62 +23,69 @@ module.exports = function (app) {
 
       let conflict = [];
 
-      if (!puzzleRegex.test(puzzle)) {
-        res.json({ error: 'Invalid characters in puzzle' })
-
-      } else if (puzzle.length !== 81) {
-        res.json({ error: 'Expected puzzle to be 81 characters long' })
-
-      } else if (!puzzle || !coordinate  || !value ) {
-        res.json({ error: 'Required field(s) missing' })
-      
-      } else if (!coodinateRegex.test(coordinate)) {
-        res.json({ error: 'Invalid coordinate'})
-
-      } else if (!valueRegex.test(value)) {
-        res.json({ error: 'Invalid value' })
-
-      } else {
-        let rowCoor = coordinate[0]
-        let colCoor = coordinate[1]
-
-        // check if value and puzzle value are the same
-        if ((solver.checkRowPlacement(puzzle, rowCoor, colCoor, value)) === "same") {
-          res.json({
-            valid: true
-          })
-
+      if (puzzle) {
+        
+        if (!puzzleRegex.test(puzzle)) {
+          res.json({ error: 'Invalid characters in puzzle' })
+  
+        } else if (puzzle.length !== 81) {
+          res.json({ error: 'Expected puzzle to be 81 characters long' })
+  
+        } else if (!coordinate  || !value ) {
+          res.json({ error: 'Required field(s) missing' })
+        
+        } else if (!coodinateRegex.test(coordinate)) {
+          res.json({ error: 'Invalid coordinate'})
+  
+        } else if (!valueRegex.test(value)) {
+          res.json({ error: 'Invalid value' })
+  
         } else {
-          // check if row contains value
-          if (solver.checkRowPlacement(puzzle, rowCoor, colCoor, value)) {
-            conflict.push("row")
-          }
-    
-          // check if columnn contains value
-          if (solver.checkColPlacement(puzzle, rowCoor, colCoor, value)) {
-            conflict.push("column")
-          }
-    
-          // check if region contains value
-          if (solver.checkRegionPlacement(puzzle, rowCoor, colCoor, value)){
-            conflict.push("region")
-          }
-
-          // generate response
-          if (conflict.length === 0) {
+          let rowCoor = coordinate[0]
+          let colCoor = coordinate[1]
+  
+          // check if value and puzzle value are the same
+          if ((solver.checkRowPlacement(puzzle, rowCoor, colCoor, value)) === "same") {
             res.json({
               valid: true
             })
-    
+  
           } else {
-            res.json({
-              valid: false,
-              conflict: conflict
-            })
-          }
-        } 
-      }
+            // check if row contains value
+            if (solver.checkRowPlacement(puzzle, rowCoor, colCoor, value)) {
+              conflict.push("row")
+            }
+      
+            // check if columnn contains value
+            if (solver.checkColPlacement(puzzle, rowCoor, colCoor, value)) {
+              conflict.push("column")
+            }
+      
+            // check if region contains value
+            if (solver.checkRegionPlacement(puzzle, rowCoor, colCoor, value)){
+              conflict.push("region")
+            }
+  
+            // generate response
+            if (conflict.length === 0) {
+              res.json({
+                valid: true
+              })
+      
+            } else {
+              res.json({
+                valid: false,
+                conflict: conflict
+              })
+            }
+          } 
+        }
 
+      } else {
+
+          res.json({ error: 'Required field(s) missing' })
+      }
+      
     });
     
   app.route('/api/solve')
